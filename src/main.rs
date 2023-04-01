400363d4068f43e484790928cca323a2d85672b5
dvnc0 <dcobb.dev@gmail.com>
2023-04-01 02:06:58 -0400

    Working on initial args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+use clap::{Command, Arg, ArgAction};
+
 fn main() {
-    println!("Hello, world!");
+    let _matches = Command::new("Suzz")
+        .version("1.0.0")
+        .author("dvnc0")
+        .about("A simple fuzzer")
+        .arg(
+            Arg::new("url")
+                .value_name("URL")
+                .help("The URL to test")
+                .required(true)
+        )
+        .arg(
+            Arg::new("file")
+                .long("file")
+                .short('f')
+                .value_name("FILE")
+                .help("Word list to use")
+                .required(true)
+        )
+        .arg(
+            Arg::new("delay")
+                .long("delay")
+                .short('d')
+                .value_name("DELAY")
+                .help("Optional delay in seconds between requests")
+                .default_value("0")
+        )
+        .arg(
+            Arg::new("verbose")
+                .long("verbose")
+                .short('v')
+                .help("Emable verbose output")
+                .action(ArgAction::SetTrue)
+        )
+        .get_matches();
 }
